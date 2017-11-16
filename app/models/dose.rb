1a51fd6427af9e8c4756cdf495142b1eb110eb17
Harrison Kim <namo918@gmail.com>
2017-11-16 15:01:32 +0800

    models complete

---
+++ app/models/dose.rb
@@ -0,0 +1,10 @@
+class Dose < ApplicationRecord
+    belongs_to :cocktail
+    belongs_to :ingredient
+    # validates :ingredient, uniqueness: true
+    validates :description, presence: true
+
+    validates :cocktail, presence: true
+    validates :ingredient, uniqueness: { scope: :cocktail }
+
+end
