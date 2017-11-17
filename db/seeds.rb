5822823799c1a8a289ad52b01879b29d58d2e498
Harrison Kim <namo918@gmail.com>
2017-11-17 12:01:39 +0800

    Add figaro - Protect my secret data in config/application.yml

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+require 'json'
+require 'open-uri'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +7,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Ingredient.create(name: "lemon")
+# Ingredient.create(name: "ice")
+# Ingredient.create(name: "mint leaves")
+
+url = 'http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+drinks_serialized = open(url).read
+drinks = JSON.parse(drinks_serialized)
+drinks['drinks'].each { |e| Ingredient.create(name: e["strIngredient1"]) }
